0fc01fbc2bcfd1a323711c45203f7f343aba9dcf
Limit <40298297+UniXOF@users.noreply.github.com>
2019-03-26 01:06:24 +0800

    add info bot

---
+++ commands/botinfo.js
@@ -0,0 +1,31 @@
+const Discord = module.require('discord.js');
+const client = new Discord.Client();
+
+module.exports.run = async (client, message, args) => 
+{
+    const embed = {
+        "description": "Инфомариция о боте...",
+        "color": 16737843,
+        "author": {
+            "name": "Bot info"
+        },
+        "thumbnail": {
+            "url": "https://pbs.twimg.com/media/D2hMcPrUgAYY1qD.png:large"
+        },
+        "fields": [
+            {
+                "name": "Количество серверов",
+                "value": `${client.guilds.size} сервер`
+            },
+            {
+                "name": "Количество пользователей",
+                "value": `${client.users.size} пользователь`
+            }
+        ]
+        };
+        message.author.send({ embed });   
+    }
+
+module.exports.help = {
+    name: "bot-info"
+}
