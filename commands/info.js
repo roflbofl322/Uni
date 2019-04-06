0dfe740d8efa851d1716b78589b96f0fc063841b
Limit <40298297+UniXOF@users.noreply.github.com>
2019-04-06 21:14:00 +0800

    user-info update

--- commands/info.js
+++ commands/info.js
@@ -4,27 +4,31 @@ var mysql = require('mysql');
 
 module.exports.run = async (client, message, args) => 
 {
-  let memberInfo = message.mentions.members.first();
-  if(!memberInfo) {
-    var userinf = new Discord.RichEmbed()
-        .setAuthor(message.author.username)
-        .setThumbnail(message.author.avatarURL)
-        .setDescription("Информация о вашем игроке")
-        .setColor(0xff6633)
-        .addField("Ник:", `${message.author.username}#${message.author.discriminator}`)
-        .addField("Дата создания вашего аккаунта:", message.author.createdAt)
-        message.channel.send(userinf);
+  con.query(`SELECT name FROM users WHERE userid = '${message.author.id}'`, function (err, rows) {
+    if (err) throw err;
+    console.log(rows[0]);
+    let memberInfo = message.mentions.members.first();
+    if(!memberInfo) {
+      var userinf = new Discord.RichEmbed()
+          .setAuthor(message.author.username)
+          .setThumbnail(message.author.avatarURL)
+          .setDescription("Информация о вашем игроке")
+          .setColor(0xff6633)
+          .addField("Игровой ник:", `${rows[0]}`)
+          .addField("Дата создания вашего аккаунта:", message.author.createdAt)
+          message.channel.send(userinf);
 
-  } else {
-    var userinfoo = new Discord.RichEmbed()
-        .setAuthor(memberInfo.displayName)
-        .setThumbnail(memberInfo.user.avatarURL)
-        .setDescription("Информация о игроке")
-        .setColor(0xff6633)
-        .addField("Ник игрока:", `${memberInfo.user.username}#${memberInfo.user.discriminator}`)
-        .addField("Дата создания аккаунта:", memberInfo.user.createdAt)
-        message.channel.send(userinfoo);
-  }
+    } else {
+      var userinfoo = new Discord.RichEmbed()
+          .setAuthor(memberInfo.displayName)
+          .setThumbnail(memberInfo.user.avatarURL)
+          .setDescription("Информация о игроке")
+          .setColor(0xff6633)
+          .addField("Ник игрока:", `${memberInfo.user.username}#${memberInfo.user.discriminator}`)
+          .addField("Дата создания аккаунта:", memberInfo.user.createdAt)
+          message.channel.send(userinfoo);
+    }
+  });
 }
 
 module.exports.help = {
