b2cbb559b19cf6b1ecdaaf80723859e32f1462bf
Limit <40298297+UniXOF@users.noreply.github.com>
2019-04-07 01:13:16 +0800

    Fix bag and add information

--- commands/info.js
+++ commands/info.js
@@ -4,34 +4,41 @@ var mysql = require('mysql');
 
 module.exports.run = async (client, message, args) => 
 {
-  con.query(`SELECT name, level FROM users WHERE userid = '${message.author.id}'`, function (err, rows) {
-    if (err) throw err;
-    console.log(rows);
-    let nick = rows[0].name;
-    let lvl = rows[0].level;
     let memberInfo = message.mentions.members.first();
     if(!memberInfo) {
-      var userinf = new Discord.RichEmbed()
-          .setAuthor(message.author.username)
-          .setThumbnail(message.author.avatarURL)
-          .setDescription("Информация о вашем игроке")
-          .setColor(0xff6633)
-          .addField("Игровой ник:", nick)
-          .addField("Уровень:", lvl)
-          .addField("Дата создания вашего аккаунта:", message.author.createdAt)
-          message.channel.send(userinf);
-
+      con.query(`SELECT name, level FROM users WHERE userid = '${message.author.id}'`, function (err, rows) {
+        if (err) throw err;
+        console.log(rows);
+        let nick = rows[0].name;
+        let lvl = rows[0].level;
+        var userinf = new Discord.RichEmbed()
+            .setAuthor(message.author.username)
+            .setThumbnail(message.author.avatarURL)
+            .setDescription("Информация о вашем игроке")
+            .setColor(0xff6633)
+            .addField("Игровой ник:", nick)
+            .addField("Уровень:", lvl)
+            .addField("Дата создания вашего аккаунта:", message.author.createdAt)
+        message.channel.send(userinf);
+      });
     } else {
+      con.query(`SELECT name, level FROM users WHERE userid = '${memberInfo.id}'`, function (err, rows) {
+      if(rows.length < 1) {
+        return message.reply("Пользователь не существует или отсутствует в базе данных.")  
+      };
+      let nick = rows[0].name;
+      let lvl = rows[0].level;
       var userinfoo = new Discord.RichEmbed()
           .setAuthor(memberInfo.displayName)
           .setThumbnail(memberInfo.user.avatarURL)
           .setDescription("Информация о игроке")
           .setColor(0xff6633)
-          .addField("Ник игрока:", `${memberInfo.user.username}#${memberInfo.user.discriminator}`)
+          .addField("Ник:", nick)
+          .addField("Уровень:", lvl)
           .addField("Дата создания аккаунта:", memberInfo.user.createdAt)
           message.channel.send(userinfoo);
+      });
     }
-  });
 }
 
 module.exports.help = {
