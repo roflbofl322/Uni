f90eea060149c7fab0959ece98cf8d58d6916d83
Limit <40298297+UniXOF@users.noreply.github.com>
2019-04-07 12:34:26 +0800

    add command

---
+++ commands/rename.js
@@ -0,0 +1,16 @@
+const Discord = module.require('discord.js');
+const client = new Discord.Client();
+var mysql = require('mysql');
+
+module.exports.run = async (client, message, args) => {
+    if(!args.join(" ")){
+        return message.channel.send(":x: " + "| Введите желаемый ник")
+    }
+    con.query(`UPDATE users SET name = '${args.join(" ")}'  WHERE userid = '${message.author.id}'`, function (err, rows) {
+        console.log(`Пользователь ${message.author.tag}, сменинил свой ник на ${args.join(" ")}.`);
+    });
+}
+
+module.exports.help = {
+    name: "rename"
+}
