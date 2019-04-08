68d751e1e9dec59b99ea3bf9e4fc63d411e8ed55
Limit <40298297+UniXOF@users.noreply.github.com>
2019-04-08 08:05:08 +0800

    index fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const fs = require('fs') // подключаем fs к файлу
-const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
 const client = new Discord.Client();
 client.discord = Discord;
@@ -34,17 +33,6 @@ client.on('ready', () => {
   })
   console.log(`Бот работает под ником: ${client.user.tag}!`);
 });
-
-const applyText = (canvas, text) => {
-	const ctx = canvas.getContext('2d');
-	let fontSize = 70;
-
-	do {
-		ctx.font = `${fontSize -= 10}px sans-serif`;
-	} while (ctx.measureText(text).width > canvas.width - 300);
-
-	return ctx.font;
-};
 
 client.commands = new Discord.Collection() // создаём коллекцию для команд
 
@@ -108,66 +96,6 @@ client.on('message', async message => {
       });
 
       if (err) throw err;
-    });
-
-    con.query(`SELECT name, level, xp, money FROM users WHERE userid = '${message.author.id}'`, async function (err, rows) {
-    let nick = rows[0].name;
-    let lvl = rows[0].level;
-    let xp = rows[0].xp;
-    let money = rows[0].money;
-
-      if(message.content === "test") {
-        //con.query(`SELECT name, level FROM users WHERE userid = '${message.author.id}'`, function (err, rows) {
-            //nick = rows[0].name;
-            //lvl = rows[0].level;
-            //if(err) throw err;
-          //});
-          const canvas = Canvas.createCanvas(1920, 1200);
-          const ctx = canvas.getContext('2d');
-        
-          const background = await Canvas.loadImage('./wallpaper.png');
-          ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-        
-          //ctx.strokeStyle = '#74037b';
-          //ctx.strokeRect(0, 0, canvas.width, canvas.height);
-        
-          //ник
-          ctx.font = applyText(canvas, `${message.author.username}`);
-          ctx.fillStyle = '#ffffff';
-          ctx.fillText(`${nick}`, canvas.width / 10.0, canvas.height / 2.25);
-
-          //level
-          ctx.font = '220px sans-serif';
-          ctx.fillStyle = '#f17556';
-          ctx.fillText(`${lvl}`, canvas.width / 2.1, canvas.height / 2.2);
-
-          //xp
-          ctx.font = '70px sans-serif';
-          ctx.fillStyle = '#f17556';
-          ctx.fillText(`${xp}`, canvas.width / 1.475, canvas.height / 2.63);
-
-          //money
-          ctx.font = '70px sans-serif';
-          ctx.fillStyle = '#f17556';
-          ctx.fillText(`${money}`, canvas.width / 1.35, canvas.height / 2.2);
-
-          //BOSS
-          ctx.font = '70px sans-serif';
-          ctx.fillStyle = '#f17556';
-          ctx.fillText('0', canvas.width / 1.4, canvas.height / 1.87);
-
-          ctx.beginPath();
-          ctx.arc(420, 230, 140, 0, Math.PI * 2, true);
-          ctx.closePath();
-          ctx.clip();
-        
-          const { body: buffer } = await snekfetch.get(message.author.displayAvatarURL);
-          const avatar = await Canvas.loadImage(buffer);
-          ctx.drawImage(avatar, 270, 70, 300, 300);
-        
-          const attachment = new Discord.Attachment(canvas.toBuffer(), 'profile.png');
-          message.channel.send(attachment);
-      }
     });
 
     let prefix = config.prefix
